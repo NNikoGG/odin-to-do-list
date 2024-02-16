b9a3d61c41ac6f6204ae3c186509babe33a80b02
NNikoGG <geekgaming222@gmail.com>
2024-02-16 17:27:27 +0530

    Render entered task to task list

--- dist/test.js
+++ dist/test.js
@@ -10,11 +10,43 @@ class Task {
       this.priority = priority;
     }
 }
-  
+
+// Display the books in the page
+function render(){
+  let taskList = document.querySelector(".task-container");
+  taskList.innerHTML = '';
+  for (let i = 0; i < myTasks.length; i++){
+      let task = myTasks[i];
+      let taskWrapper = document.createElement('div');
+      taskWrapper.className = "task";
+      taskList.appendChild(taskWrapper);
+      let taskName = document.createElement('p');
+      taskName.innerText = task.title;
+      let taskDescription = document.createElement('p');
+      taskDescription.innerText = task.description;
+      let taskDate = document.createElement('p');
+      taskDate.innerText = task.date;
+      let taskPriority = document.createElement('p');
+      taskPriority.innerText = task.priority;
+      let removeButton = document.createElement('button');
+      removeButton.innerText = "X";
+      removeButton.className = "remove-button";
+      removeButton.dataset.index = i;
+
+      taskWrapper.appendChild(taskName);
+      taskWrapper.appendChild(taskDescription);
+      taskWrapper.appendChild(taskDate);
+      taskWrapper.appendChild(taskPriority);
+      taskWrapper.appendChild(removeButton);
+  }
+}
+
+
 function addTasksToMyTasks(title, description, date, priority) {
     let newTask = new Task(title, description, date, priority);
     myTasks.push(newTask);
     console.log(myTasks);
+    render();
 }
 
 let taskForm = document.querySelector('#task-form');
@@ -30,6 +62,22 @@ taskForm.addEventListener('submit', function(event){
     taskForm.reset();
     formDialog.close();
 })
+
+// Remove button
+function remove(index){
+  myTasks.splice(index, 1);
+  render();
+  console.log(myTasks);
+}
+
+document.addEventListener('click', function(event){
+  if (event.target && event.target.className == 'remove-button') {
+      let index = event.target.dataset.index; // Get the index from the data attribute
+      remove(index);
+  }
+});
+
+
 
 // Display the task form when pressing the add task button
 let addTaskButton = document.querySelector('#add-task-button');
