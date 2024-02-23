b837e0bdad1875ec3028a2d97acf5c729dbf4726
NNikoGG <geekgaming222@gmail.com>
2024-02-23 10:37:13 +0530

    Fix file pathing

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/modules/index.js',
+  entry: './src/index.js',
   mode: 'development',
   devtool: 'inline-source-map',
   output: {
